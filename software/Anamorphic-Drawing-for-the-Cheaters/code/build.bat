983db5a732c38fb31f9fbcaa73285e6d2ba680b3
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2019-02-28 19:07:21 -0500

    Anamorphic-Drawing-for-the-Cheaters: code replaced with zip

--- software/Anamorphic-Drawing-for-the-Cheaters/code/build.bat
+++
@@ -1,6 +0,0 @@
-::modify this path to use appropriate version of MSBuild:
-set tool=%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe
-set solution=AnamorphicDrawing.sln
-
-::%tool% %solution% /p:Configuration=Debug
-%tool% %solution% /p:Configuration=Release
